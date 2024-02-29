02665ad83dd56aa9aefaa632afc4035e661ed369
Mohit-0918 <mohit.sharma.2001rip@outlook.com>
2024-03-01 01:18:29 +0530

    Updated Resources

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './resources/LegalConnect.svg';
+import './CSS/App.css';
 
 function App() {
   return (
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          LegalConnect
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Mohit-0918/LegalConnect"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Where Law Meets Connection, Solution Unfolds
         </a>
       </header>
     </div>
