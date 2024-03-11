bb1731fae6bcc9d5d8fa2e8c21c87f6ef186c777
Mohit-0918 <mohit.sharma.2001rip@outlook.com>
2024-03-12 01:21:57 +0530

    bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./pages/Register";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import PersonalProfile from "./pages/lawyerprofile.jsx"
 import LoginL from "./pages/LoginL.jsx"
